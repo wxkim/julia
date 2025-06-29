10a7de34c1740870ed34a116cfcfd22ae233603b
wxkim <will.kim0526@gmail.com>
2025-06-29 00:14:41 -0500

    Window render prototyping

--- display/render.c
+++ display/render.c
@@ -5,7 +5,8 @@ static int window_width, window_length;
 void render_init(int width, int height) {
     win_width = width;
     win_height = height;
-
+    glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
+    buffer_init(width, height);
     
 }
 
@@ -14,12 +15,22 @@ void render_frame(void) {
     win_width = width;
     win_height = height;
 
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, width, 0, height);
+    glMatrixMode(GL_MODELVIEW);
+    buffer_resize(width, height);
 }
 
 
 void render_resize(int width, int height) {
+    glClear(GL_COLOR_BUFFER_BIT);
 
+    glRasterPos2i(0, 0);
+    glDrawPixels(win_width, win_height, GL_RGB, GL_UNSIGNED_BYTE, buffer_pixels());
 
+    glutSwapBuffers();
 }
 
 
--- inc/main.h
+++ inc/main.h
@@ -2,4 +2,4 @@
 #include "stdio.h"
 #include "complex.h"
 #include "core.h"
-#include "display/render.h"
+#include "render.h"
